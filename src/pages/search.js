4b1e24a250417ea8570c57aee896b99abded77ac
donghyun960215 <tlzma789@naver.com>
2024-05-20 22:51:48 +0900

    기본 데이터 가져오기 확인 완료

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,6 +1,22 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
+import axios from 'axios'
 
 const Search = () => {
+  const OMDB_API_KEY = '7035c60c'
+  const [movieData, setMovieData] = useState()
+
+  const getMovieData = async () => {
+    //const response = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`);
+    const response = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=jobs`);
+    setMovieData(response);
+    console.log(response);
+  }
+  useEffect(() => {
+    if(!movieData){
+      getMovieData();
+    }
+  }, [movieData]);
+
   return  (
     <div>
       <p>Search</p>
