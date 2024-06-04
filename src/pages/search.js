06b84b459edf9af4eaa966fbb1a6ce1af22bcda7
donghyun960215 <tlzma789@naver.com>
2024-06-04 22:39:27 +0900

    css 작업 중

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react"
 import axios, {patch} from 'axios'
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const Search = () => {
   const navigate = useNavigate();
@@ -39,6 +40,7 @@ const Search = () => {
       setPage(page+1);
       const response = await axios.get(`https://www.omdbapi.com/?apikey=7035c60c&s=${title}&type=${type}&y=${selectedYear}&page=${page}`);
       setMovieData(movieData.concat(response.data.Search));
+      console.log(movieData);
       setFetching(true);
     }
     setFetching(false);
@@ -55,44 +57,62 @@ const Search = () => {
 
   return  (
     <>
-    <form onSubmit={onsubmit}>
-      <div>
-        <input
-          className="text"
-          type={"text"}
-          id={"title"}
-        />
+      <div className="container">
+        <h1>
+          <span>OMDb API</span><br/>
+          THE OPEN<br/>
+          MOVIE DATABASE
+        </h1>
+        <p>
+          The OMDb API is a RESTful web service to obtain movie information, all content and images on the site are
+          contributed and maintained by our users.<br/>
+          If you find this service useful, please consider making a one-time donation or become a patron.
+        </p>
       </div>
-      <div>
-        <select id={"type"}>
-          <option key="movie" value="movie">movie</option>
-          <option key="series" value="series">series</option>
-          <option key="episode" value="episode">episode</option>
-        </select>
-      </div>
-      <div>
-        <select id={"yearList"} onChange={(e) => setSelectedYear(e.target.value)}>
-        {yearSelectData.map((item, i) => (
-          <option defaultValue={"All year"} value={item} key={i}>{item}</option>
-        ))}
-        </select>
-      </div>
-      <div>
-        <button type={"submit"}>등록</button>
-      </div>
-    </form>
+      <form onSubmit={onsubmit} className="form-inline">
+        <div>
+          <input
+            className="text"
+            type={"text"}
+            id={"title"}
+            placeholder="Search for Movies, Series & more"
+          />
+        </div>
+        <div className="selects">
+        <div>
+          <select id={"type"} className="select">
+            <option key="movie" value="movie">movie</option>
+            <option key="series" value="series">series</option>
+            <option key="episode" value="episode">episode</option>
+          </select>
+        </div>
+        <div>
+          <select id={"yearList"} onChange={(e) => setSelectedYear(e.target.value)}>
+            {yearSelectData.map((item, i) => (
+              <option defaultValue={"All year"} value={item} key={i}>{item}</option>
+            ))}
+          </select>
+        </div>
+        </div>
+        <div>
+          <button type={"submit"}>등록</button>
+        </div>
+      </form>
       {movieData && movieData?.map((item, i) => (
-        <>
+        <div className="movie">
           <img src={item.Poster}
+               className="poster"
                alt="poster"
                width={100}
                height={150}
                key={item.imdbID}
                onClick={() => goToDetail(item.imdbID)}
           />
-          <p> {item.Title}</p>
-          <p>{item.Year}</p>
-        </>
+          <div className="info">
+            <div className="year">{item.Year}</div>
+            <div className="title">{item.Title}</div>
+          </div>
+        </div>
       ))}
     </>
   )
